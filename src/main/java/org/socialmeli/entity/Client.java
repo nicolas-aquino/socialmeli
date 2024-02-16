719d7d1a86e8c4136eccf564408aab69d5b44a38
osjuarez <oscar.juarez@mercadolibre.com>
2024-02-16 17:27:21 -0300

    Changed client lombok definition

--- src/main/java/org/socialmeli/entity/Client.java
+++ src/main/java/org/socialmeli/entity/Client.java
@@ -1,13 +1,8 @@
 package org.socialmeli.entity;
 
+
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.util.List;
-
-@Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Client extends User {
 }
