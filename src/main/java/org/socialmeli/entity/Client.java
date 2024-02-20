426a201de1b0136fcf1ec6d085e4f465217ae2b6
Nicolas Aquino <nicolassergio.aquino@mercadolibre.com>
2024-02-20 11:27:39 -0300

    Fix US_0001

--- src/main/java/org/socialmeli/entity/Client.java
+++ src/main/java/org/socialmeli/entity/Client.java
@@ -3,7 +3,10 @@ package org.socialmeli.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
+
 @Data
 @AllArgsConstructor
 public class Client extends User {
+
 }
--- src/main/java/org/socialmeli/entity/User.java
+++ src/main/java/org/socialmeli/entity/User.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public abstract class User {
     protected Integer userId;
     protected String userName;
     protected List<Vendor> following;
+
+    public User() {
+        this.userId = null;
+        this.userName = null;
+        this.following = new ArrayList<>();
+    }
 }
