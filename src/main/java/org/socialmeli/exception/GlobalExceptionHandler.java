0a9e90a1ee3ab2f3877e96371725e5c7160fb3ab
osjuarez <oscar.juarez@mercadolibre.com>
2024-02-29 09:42:59 -0300

    validaciones devuelven todos los errores donde se producen los fallos

--- src/main/java/org/socialmeli/exception/GlobalExceptionHandler.java
+++ src/main/java/org/socialmeli/exception/GlobalExceptionHandler.java
@@ -4,8 +4,10 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.socialmeli.dto.response.ExceptionDto;
+import org.socialmeli.dto.response.NotValidExceptionDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.List;
 
 @ControllerAdvice(annotations = RestController.class)
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -40,6 +44,25 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handlerMethodValidationException(ConstraintViolationException e) {
 
         ExceptionDto exceptionDto = new ExceptionDto(e.getLocalizedMessage());
+        return new ResponseEntity<>(exceptionDto,  HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handlerMethodValidationException(HttpMessageNotReadableException e) {
+
+        ExceptionDto exceptionDto = new ExceptionDto(e.getLocalizedMessage());
+        return new ResponseEntity<>(exceptionDto,  HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handlerMethodValidationException(MethodArgumentNotValidException e) {
+
+        //List<ExceptionDto> exceptionsDto = e.getBindingResult().getAllErrors().stream().map(error -> new ExceptionDto( error.getDefaultMessage())).toList();
+
+        //ExceptionDto exceptionDto = new ExceptionDto(e.getBindingResult().getFieldError().getDefaultMessage());
+        ExceptionDto exceptionDto = new ExceptionDto(e.getDetailMessageArguments()[1].toString());
+
+
         return new ResponseEntity<>(exceptionDto,  HttpStatus.BAD_REQUEST);
     }
 }
